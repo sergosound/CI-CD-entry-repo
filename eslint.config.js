32ee425ae0c9ed0d97baf93fb834ac41730ff8ed
Serhii Dirko <liptoncofe2012@gmail.com>
2025-08-26 17:18:54 +0300

    fix eslint outdated rule

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,27 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  // Global ignores
+  {
+    ignores: ["dist/**"],
+  },
+  // Global settings
+  {
+    settings: { react: { version: "detect" } },
+  },
+  // React recommended rules
   pluginReact.configs.flat.recommended,
+  // Project rules and overrides
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+  },
 ]);
